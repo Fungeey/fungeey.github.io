380d24e3008147770d70eeae0e1d6b5c400c65b2
Aaron Lee <45613337+Fungeey@users.noreply.github.com>
2022-10-12 14:28:08 -0400

    fix img overlapping when load

--- main.js
+++ main.js
@@ -1,39 +1,49 @@
 
-
-let state = 0;
-
-function activate(type, tags) {
-    type.style.backgroundColor = "var(--green)";
-    for(let i = 0; i < tags.length; i++) {
-        tags[i].style.backgroundColor = "var(--green)";
-        tags[i].style.opacity = "100%";
-    }
-}
-
-function deactivate(type, tags) {
-    type.style.backgroundColor = "var(--grey)";
-    for(let i = 0; i < tags.length; i++) {
-        tags[i].style.backgroundColor = "var(--grey)";
-        tags[i].style.opacity = "70%";
-    }
-}
-
-let langs = document.getElementsByClassName("l");
-let l = document.getElementById("languages");
+// Reload Masonry when images are loaded
+let grid = document.querySelector('.projectGrid');
+imagesLoaded(grid, () => {
+    let masonry = new Masonry(grid, {
+        itemSelector: '.grid-item',
+        columnWidth: 300
+    })
+})
 
 
-let tech = document.getElementsByClassName("t");
-let t = document.getElementById("technologies");
+
+// let state = 0;
+
+// function activate(type, tags) {
+//     type.style.backgroundColor = "var(--green)";
+//     for(let i = 0; i < tags.length; i++) {
+//         tags[i].style.backgroundColor = "var(--green)";
+//         tags[i].style.opacity = "100%";
+//     }
+// }
+
+// function deactivate(type, tags) {
+//     type.style.backgroundColor = "var(--grey)";
+//     for(let i = 0; i < tags.length; i++) {
+//         tags[i].style.backgroundColor = "var(--grey)";
+//         tags[i].style.opacity = "70%";
+//     }
+// }
+
+// let langs = document.getElementsByClassName("l");
+// let l = document.getElementById("languages");
 
 
-l.onclick = () => {
-    activate(l, langs);
-    deactivate(t, tech);
-}
+// let tech = document.getElementsByClassName("t");
+// let t = document.getElementById("technologies");
 
-l.onclick();
 
-t.onclick = () => {
-    activate(t, tech);
-    deactivate(l, langs);
-}
+// l.onclick = () => {
+//     activate(l, langs);
+//     deactivate(t, tech);
+// }
+
+// l.onclick();
+
+// t.onclick = () => {
+//     activate(t, tech);
+//     deactivate(l, langs);
+// }
