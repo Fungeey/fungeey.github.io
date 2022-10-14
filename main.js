6c1e3640de70926313e6f4a599dcbb93c0e23e6e
Aaron Lee <45613337+Fungeey@users.noreply.github.com>
2022-10-14 00:38:26 -0400

    redesign front

--- main.js
+++ main.js
@@ -6,9 +6,39 @@ imagesLoaded(grid, () => {
         itemSelector: '.grid-item',
         columnWidth: 300
     })
+    
+    unfade(document.querySelector('#projects'));
 })
 
 
+function unfade(element) {
+    var op = 0.1;  // initial opacity
+    element.style.display = 'block';
+    var timer = setInterval(function () {
+        if (op >= 1){
+            clearInterval(timer);
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.1;
+    }, 10);
+}
+
+
+
+// let options = {preset: "bigCircles"};
+// tsParticles.load('body', options);
+// (async () => {
+//     await tsParticles.load("body", 
+//     {
+//         particles:{
+//             move:{
+//                 enabled:true
+//             },
+//         }
+//     }
+//     );
+//   })();
 
 // let state = 0;
 
